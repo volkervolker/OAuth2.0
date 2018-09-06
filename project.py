0e0934152232373e17b86cd36dcb619045cf28f1
vagrant <vagrant@vagrant.vm>
2018-09-06 21:09:19 +0000

    Added lesson 4 stuff

--- project.py
+++ project.py
@@ -26,6 +26,99 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['credentials']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        flash("mr you are fuggin logged outta!")
+    else:
+        flash("u have not been logged in!")
+    return redirect(url_for('showRestaurants'))
+        
+    
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid sate parameter.'), 401)
+        reponse.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    userinfo_url = 'https://graph.facebook.com/v3.1/me'  # ???
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    url = 'https://graph.facebook.com/%s/permissions' % facebook_id
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    #del login_session['username']
+    #del login_session['email']
+    #del login_session['picture']
+    #del login_session['user_id']
+    #del login_session['facebook_id']
+    return "logged out hell yeah!"
+    
+
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     if request.args.get('state') != login_session['state']:
@@ -72,7 +165,11 @@ def gconnect():
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
+    print "====="
+    print data
+    print "====="
 
+    login_session['provider'] = 'google'
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
@@ -113,11 +210,11 @@ def gdisconnect():
     result = h.request(url, 'GET')[0]
 
     if result['status'] == "200":
-        del login_session['credentials']
-        del login_session['gplus_id']
-        del login_session['username']
-        del login_session['email']
-        del login_session['picture']
+        #del login_session['credentials']
+        #del login_session['gplus_id']
+        #del login_session['username']
+        #del login_session['email']
+        #del login_session['picture']
 
         response = make_response(json.dumps('User logged out.'), 200)
         response.headers['Content-Type'] = 'application/json'
@@ -211,7 +308,7 @@ def deleteRestaurant(restaurant_id):
 def showMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
-    if login_session['user_id'] is not restaurant.user_id:
+    if 'user_id' not in login_session or login_session['user_id'] is not restaurant.user_id:
         creator = getUserInfo(restaurant.user_id)
         return render_template('publicmenu.html', items = items, restaurant = restaurant, creator = creator)
     return render_template('menu.html', items = items, restaurant = restaurant)
@@ -222,7 +319,7 @@ def showMenu(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>/menu/new/',methods=['GET','POST'])
 def newMenuItem(restaurant_id):
   restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-  if login_session['user_id'] is not restaurant.user_id:
+  if 'user_id' not in login_session or login_session['user_id'] is not restaurant.user_id:
             return redirect(url_for('showMenu', restaurant_id = restaurant_id))
   if request.method == 'POST':
       newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], course = request.form['course'], restaurant_id = restaurant_id)
@@ -239,7 +336,7 @@ def editMenuItem(restaurant_id, menu_id):
 
     editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    if login_session['user_id'] is not restaurant.user_id:
+    if 'user_id' not in login_session or login_session['user_id'] is not restaurant.user_id:
             return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     if request.method == 'POST':
         if request.form['name']:
@@ -262,7 +359,7 @@ def editMenuItem(restaurant_id, menu_id):
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods = ['GET','POST'])
 def deleteMenuItem(restaurant_id,menu_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    if login_session['user_id'] is not restaurant.user_id:
+    if 'user_id' not in login_session or login_session['user_id'] is not restaurant.user_id:
             return redirect(url_for('showMenu', restaurant_id = restaurant_id))
     itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one() 
     if request.method == 'POST':
@@ -295,4 +392,5 @@ def getUserID(email):
 if __name__ == '__main__':
   app.secret_key = 'super_secret_key'
   app.debug = True
-  app.run(host = '0.0.0.0', port = 5000)
+  app.run(host = '0.0.0.0', port = 5000, ssl_context='adhoc')
+  # app.run(host = '0.0.0.0', port = 5000)
